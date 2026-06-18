ed689243aa28e1c4fe8aa5ab821a5fc23b42f026
Justin Chen <justin.dychen@gmail.com>
2026-06-18 21:18:52 +0800

    class 3: containter設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,30 @@
 import 'package:flutter/material.dart';
-void main() => runApp(MyApp());
 
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
-        body: const Center(
-          child: Text(
-            '我愛你媽媽，我想當你爸爸，哈哈哈哈哈哈哈哈哈哈哈，我愛你媽媽，我想當你爸爸，哈哈哈哈哈哈哈哈哈哈哈，我愛你媽媽，我想當你爸爸，哈哈哈哈哈哈哈哈哈哈哈，我愛你媽媽，我想當你爸爸，哈哈哈哈哈哈哈哈哈哈哈 ',
-            textAlign: TextAlign.left,
-            overflow: TextOverflow.fade,
-            style: TextStyle(
-              fontSize: 25.0,
-              color: Color.fromARGB(255, 255, 125, 125),
-              decoration: TextDecoration.underline,
-              decorationStyle: TextDecorationStyle.solid,
-            ),
-            maxLines: 1,
-          ),
+        body: Center(
+          child: Container(
+            alignment: Alignment.center, // 💡 設定 Container 內部的對齊方式
+            width: 500.0,
+            height: 400.0,
+            color: Color.fromARGB(255,0, 0, 0), // 💡 設定 Container 的背景顏色
+            child: Text( // 💡 順便幫你拿掉了現代寫法不需要的 new
+              'Hello World',
+              style: TextStyle(fontSize: 25.0, color: Colors.pink),
+            ), // 👈 這是 Text 的結束括號
+          ), // 👈 這裡原本少了一個結束 Container 的括號！
         ),
       ),
     );
   }
-}
+} 
 
-//style: TextStyle(fontSize: 25.0, color: Colors.pink), 設定文字樣式，這裡設定字體大小為25.0，顏色為粉紅色
-
-
-//maxLines: 1, 設定最大行數為1行，超出部分會根據overflow的設定來處理
-//overflow: TextOverflow.fade, 超出部分淡出，顯示為...，如果不設定overflow，則會顯示所有行數
-//overflow: TextOverflow.ellipsis, 超出部分顯示為...，如果不設定overflow，則會顯示所有行數
-//textAlign: TextAlign.center, 中間對其， TextAlign.left, 靠左對其， TextAlign.right, 靠右對其， TextAlign.justify, 兩端對齊
+//alignment: Alignment.center, // 💡 設定 Container 內部的對齊方式
+//alignment: Alignment.bottomCenter, // 💡 設定 Container 內部的對齊方式
+//alignment: Alignment.bottomRight, // 💡 設定 Container 內部的對齊方式
