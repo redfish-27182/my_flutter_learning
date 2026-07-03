08c2a5583fa1b708fdf2e99335021b2c8b280296
Justin Chen <justin.dychen@gmail.com>
2026-07-03 14:23:58 +0800

    class 8: row水平布局

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,40 @@
 import 'package:flutter/material.dart';
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'GridView 圖片展示', 
-
+      title: 'Row Widget Demo',
       home: Scaffold(
-        appBar: AppBar(title: const Text('GridView 圖片展示'),),
-        // 💡 修正：外層直接用 ListView，讓整頁可以上下滾動
-        body:GridView(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3, // 每行顯示兩個格子
-            mainAxisSpacing: 4.0, // 主軸間距
-            crossAxisSpacing: 2.0, // 交叉軸間距               
-            childAspectRatio: 0.7, // 格子的寬高比
-          ),
-        children: <Widget>[
-          new Image.network('https://picsum.photos/250?image=9', fit: BoxFit.cover), 
-          // fit: BoxFit.cover 讓圖片填滿格子
-          new Image.network('https://picsum.photos/250?image=10', fit: BoxFit.cover),
-          new Image.network('https://picsum.photos/250?image=11', fit: BoxFit.cover),
-          new Image.network('https://picsum.photos/250?image=12', fit: BoxFit.cover),
-          new Image.network('https://picsum.photos/250?image=13', fit: BoxFit.cover),
-          new Image.network('https://picsum.photos/250?image=14', fit: BoxFit.cover),
-          new Image.network('https://picsum.photos/250?image=15', fit: BoxFit.cover),
-
-        ],
-        )
+        appBar: AppBar(
+          title: const Text('Row Widget Demo'),
+        ),
+        body: Row( 
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue, // Set the background color
+              ),
+              child: const Text('Blue Button'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red, // Set the background color
+              ),
+              child: const Text('Red Button'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green, // Set the background color
+              ),
+              child: const Text('Green Button'),
+            ),
+          ],
+        ),
       ),
     );
   }
