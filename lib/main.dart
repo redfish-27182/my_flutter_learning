319257e451cb75b30242efaa0c2ce0ed36c02a8d
Justin Chen <justin.dychen@gmail.com>
2026-07-03 19:09:01 +0800

    class 13: Navigator 導航組件

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,44 @@
 import 'package:flutter/material.dart';
-void main() => runApp(MyApp());
+void main() {
+  runApp(MaterialApp(
+    title:'導航測試',
+    home: FirstScreen(),
+  ));
+}
 
-class MyApp extends StatelessWidget {
+class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var card = Card(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          ListTile(
-            title: Text('陳鼎云', style: TextStyle(fontSize: 20.0)),
-            subtitle: Text('國立臺灣大學'),
-            leading: Icon(Icons.account_box, color: Colors.lightBlue),
-          ),
-
-          ListTile(
-            title: Text('陳姮予', style: TextStyle(fontSize: 20.0)),
-            subtitle: Text('國立陽明交通大學'),
-            leading: Icon(Icons.account_box, color: Colors.lightBlue),
-          ),
-
-          ListTile(
-            title: Text('陳俊成', style: TextStyle(fontSize: 20.0)),
-            subtitle: Text('國立清華大學'),
-            leading: Icon(Icons.account_box, color: Colors.lightBlue),
-          ),
-      ],
-
-    );
-
-    return MaterialApp(
-      title: 'Stack 布局測試',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('垂直方向布局'), 
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('第一頁'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('前往第二頁'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondScreen()),
+            );
+          },
         ),
-        body: Center(
-          child: card,
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar( title: Text('第二頁')),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('返回第一頁'),
+          onPressed: () {
+            Navigator.pop(context);
+          },
         ),
       ),
     );
