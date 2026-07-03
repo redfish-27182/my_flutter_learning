de4aaa471f2868129f3f17fa95b75938e890e7ff
Justin Chen <justin.dychen@gmail.com>
2026-07-03 17:28:36 +0800

    class 10: Stack 層疊組件ˇ

--- lib/main.dart
+++ lib/main.dart
@@ -4,25 +4,41 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var stack = Stack(
+      alignment: FractionalOffset(0.5,0.8),
+      // 調整子組件在交叉軸方向的對齊方式
+      children: <Widget>[
+        // 背景图片
+        CircleAvatar(
+          backgroundImage: NetworkImage(
+              'https://img1.baidu.com/it/u=1871559050,1680910223&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500'),
+          radius: 100,
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.lightBlue,
+          ),
+          padding: EdgeInsets.all(10.0),
+          child: Text(
+            '我是陳鼎云',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+            ),
+          ),
+        )
+      ],
+    );
+
     return MaterialApp(
-      title: 'Row Widget Demo',
+      title: 'Stack 布局測試',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('垂直方向布局'),
         ),
-        body: Center(child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          //mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              color: Colors.red,
-              height: 100.0,
-              width: 500.0,
-            ),
-            Text('我是陳鼎云'),
-            Text('我愛淋雨'),
-          ],
-        )
+        body: Center(
+          child: stack,
+        ),
       ),
     );
   }
