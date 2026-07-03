0a21d891b200013d3b3a19cb3f53ae65e437b1d2
Justin Chen <justin.dychen@gmail.com>
2026-07-03 23:18:10 +0800

    class 14: 傳遞參數

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,50 @@
 import 'package:flutter/material.dart';
+
+class Product {
+  final String title; // 商品標題
+  final String description; // 商品描述
+  Product({required this.title, required this.description});
+}
+
 void main() {
   runApp(MaterialApp(
-    title:'導航測試',
-    home: FirstScreen(),
+    title: '商品列表',
+    home: Productlist(
+      products: List.generate(
+        20,
+        // 💡 修正：加上 title: 和 description: 標籤，完美對齊你的資料結構！
+        (i) => Product(
+          title: '商品 $i', 
+          description: '這是商品 $i 的描述',
+        ),
+      ), 
+    ),
   ));
 }
 
-class FirstScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('第一頁'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text('前往第二頁'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => SecondScreen()),
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
+class Productlist extends StatelessWidget {
+  final List<Product> products; // 商品列表
+  
+  // 💡 提示：未來這裡可以用更現代的 const Productlist({super.key, ...}) 寫法，藍色虛線會更少喔！
+  Productlist({Key? key, required this.products}) : super(key: key); 
 
-class SecondScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // 💡 修正：把原本打錯的 return: 改回正確的 return
     return Scaffold(
-      appBar: AppBar( title: Text('第二頁')),
-      body: Center(
-        child: ElevatedButton(
-          child: Text('返回第一頁'),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
+      appBar: AppBar(title: const Text('商品列表')),
+      // 💡 修正：把原本的 ListView 改成 ListView.builder，這樣可以更有效率地生成列表
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(products[index].title),
+            subtitle: Text(products[index].description),
+            
+            onTap:(){}
+          );
+        },
       ),
     );
-  }
+  }    
 }
